3d3d10bd4f7b25d106ea2914664cf4496cd4d74b
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-05-24 18:18:18 +0800

    change package import path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from './node_modules/uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const html = document.documentElement;
 const selectThemeButton = document.getElementById('themeSelector');
