3b99fcc0a97b5358e785ce5429d9e75d55b63fba
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-05-26 16:18:03 +0800

    feat: add drag and drop function

--- index.js
+++ index.js
@@ -42,12 +42,16 @@ window.addEventListener('resize', () => {
 const removeStyleAttribute = (elements) => {
   document.querySelectorAll(elements).forEach((element) => {
     element.removeAttribute('style');
+    element.classList.remove('filtered');
   });
 };
 
 const setDisplayNone = (elements) => {
   document.querySelectorAll(elements).forEach((element) => {
-    element.style.display = 'none';
+    element.classList.add('filtered');
+    setTimeout(() => {
+      element.style.display = 'none';
+    }, 350);
   });
 };
 
@@ -103,8 +107,11 @@ const updateContainerDisplay = () => {
 };
 
 const removeTaskFromList = (parentElement, element) => {
-  parentElement.removeChild(element);
-  updateContainerDisplay();
+  element.classList.add('removing');
+  setTimeout(() => {
+    parentElement.removeChild(element);
+    updateContainerDisplay();
+  }, 350);
 };
 
 const createTaskElement = (inputText) => {
@@ -118,6 +125,14 @@ const createTaskElement = (inputText) => {
   let id = uuidv4();
 
   li.classList.add('list__item', 'task');
+  li.setAttribute('draggable', 'true');
+
+  li.addEventListener('dragstart', () => {
+    li.classList.add('dragging');
+  });
+  li.addEventListener('dragend', () => {
+    li.classList.remove('dragging');
+  });
 
   label.setAttribute('for', id);
   label.classList.add('sr-only');
@@ -192,3 +207,26 @@ selectThemeButton.addEventListener('click', () => {
     themeLogo.removeAttribute('style');
   }
 });
+
+taskList.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  const dragElement = document.querySelector('.dragging');
+  const afterElement = getDragAfterElement(taskList, e.clientY);
+  if (afterElement == null) {
+    taskList.appendChild(dragElement);
+  } else {
+    taskList.insertBefore(dragElement, afterElement);
+  }
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll("li[draggable='true']"),
+  ];
+  return (
+    draggableElements.find((element) => {
+      const box = element.getBoundingClientRect();
+      return y < box.top + box.height / 2;
+    }) || null
+  );
+}
